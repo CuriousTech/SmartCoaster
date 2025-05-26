a0e2cf0064e0ae72d5d404cd9d85ef73c6e8ce32
Greg Cunningham <greg@curioustech.net>
2025-05-26 13:51:38 -0400

    Update Prefs.h

--- Scale/Prefs.h
+++ Scale/Prefs.h
@@ -15,13 +15,13 @@ public:
 
   uint16_t  size = EESIZE;          // if size changes, use defauls
   uint16_t  sum = 0xAAAA;           // if sum is diiferent from memory struct, write
-  char      szSSID[32] = "SSID"; // SSID of router
+  char      szSSID[32] = ""; // SSID of router (blank for EspTouch)
   char      szSSIDPassword[64] = "password"; // password for router
   char      szName[32] = "Scale1"; // mDNS and OTA name
   uint8_t   hostIP[4] = {192,168,31,100}; // Use to send JSON updates to a host
   uint16_t  hostPort = 80;
   uint16_t  sendRate = 15; // rate sent to host in seconds
-  uint16_t  logRate = 60; // chart logging rate in seconds
+  uint16_t  logRate = 10; // chart logging rate in seconds
   uint16_t  displayRate = 500; // web page update in ms
   float     fCal = 0.477f;
   int16_t   calWt = 50; // default 50g calibration weight
