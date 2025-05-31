e89982f3c52782a17b94cfd7d1c39460b2d3d68c
Greg Cunningham <greg@curioustech.net>
2025-05-31 02:16:48 -0400

    Update weightArray.h

--- Scale/weightArray.h
+++ Scale/weightArray.h
@@ -27,7 +27,8 @@ public:
   WeightArray(){};
   void init(void);
   void saveData(void);
-  void newDay(int8_t day);
+  void newDay(void);
+  uint8_t localHour(void);
   void add(uint32_t date, int32_t value, AsyncWebSocket &ws, int WsClientID);
   void historyDump(bool bStart, AsyncWebSocket &ws, int WsClientID);
 
